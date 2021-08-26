8fecb5e135670975e55e021668b7925e4a76802f
James Fargher <jfargher@gitlab.com>
2021-08-26 13:45:11 +1200

    Add FetchBundle to repository service proto
    FetchBundle will allow updating an existing repository with the objects
    and refs of a bundle, such as an incremental backup. Right now the only
    other RPC is `CreateRespositoryFromBundle` but this requires the
    repository to not already exist. For incremental backups, there will be
    a series of bundles that need to be applied to a repository in a
    specific order.

--- ruby/proto/gitaly/repository-service_services_pb.rb
+++ ruby/proto/gitaly/repository-service_services_pb.rb
@@ -42,6 +42,8 @@ module Gitaly
       rpc :CreateBundle, Gitaly::CreateBundleRequest, stream(Gitaly::CreateBundleResponse)
       # CreateBundleFromRefList creates a bundle from a stream of ref patterns
       rpc :CreateBundleFromRefList, stream(Gitaly::CreateBundleFromRefListRequest), stream(Gitaly::CreateBundleFromRefListResponse)
+      # FetchBundle fetches references from a bundle into the local repository.
+      rpc :FetchBundle, stream(Gitaly::FetchBundleRequest), Gitaly::FetchBundleResponse
       rpc :CreateRepositoryFromBundle, stream(Gitaly::CreateRepositoryFromBundleRequest), Gitaly::CreateRepositoryFromBundleResponse
       # GetConfig reads the target repository's gitconfig and streams its contents
       # back. Returns a NotFound error in case no gitconfig was found.
